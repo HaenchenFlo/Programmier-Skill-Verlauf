c059e60c72ad5edd4538c52b6b1fd70176782cfb
HaenchenFlo <haenchenflorian@gmail.com>
2025-02-14 22:49:16 +0100

    Aufgabe 1 + 2 fertig; fehlt noch Testklasse für Aufgabe 3

--- src/AB3/Aufgabe1/Kunde.java
+++ src/AB3/Aufgabe1/Kunde.java
@@ -1,5 +1,21 @@
 package AB3.Aufgabe1;
 
 public class Kunde {
+    private String name;
+    private int kundennummer;
+    private static int nummerZaehler = 1;
 
+    public Kunde(String name) {
+        this.name = name;
+        this.kundennummer = nummerZaehler;
+        nummerZaehler++;
+    }
+
+    public String toString() {
+        return "Name: " + this.name + ", Kundennummer: " + this.kundennummer;
+    }
+
+    public static int anzahlKunden() {
+        return nummerZaehler - 1;
+    }
 }
