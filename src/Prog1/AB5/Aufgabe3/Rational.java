e458e7034cdbd01f21f96d9f5a1de94015381c15
HaenchenFlo <haenchenflorian@gmail.com>
2025-02-26 13:19:02 +0100

    Aufgabe 3 AB5 angefangen mit Rationaltest erstellung

--- src/Prog1/AB5/Aufgabe3/Rational.java
+++ src/Prog1/AB5/Aufgabe3/Rational.java
@@ -1,4 +1,27 @@
 package Prog1.AB5.Aufgabe3;
 
 public class Rational {
+    private int zaehler; //Obere Zahl
+    private int nenner; // Untere Zahl
+    private int ggT;
+
+
+    //Konstruktor
+    public Rational(int zaehler, int nenner) {
+        this.zaehler = zaehler;
+        this.nenner = nenner;
+    }
+
+    public Rational(int zaehler) {
+        this.zaehler = zaehler;
+        this.nenner = 1;
+    }
+
+    public void kuerzen(int zaehler, int nenner) {
+        if(zaehler == 0) {
+            this.nenner = 1;
+            return;
+        }
+
+    }
 }
---
+++ src/Prog1/AB5/Aufgabe3/RationalTest.java
@@ -0,0 +1,4 @@
+package Prog1.AB5.Aufgabe3;
+
+public class RationalTest {
+}
