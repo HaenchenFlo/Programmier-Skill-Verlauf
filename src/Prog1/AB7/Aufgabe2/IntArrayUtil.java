dd6f8aabbeda68b1f7e69f264a45b6bc1c5ab8f6
HaenchenFlo <haenchenflorian@gmail.com>
2025-03-04 16:38:47 +0100

    Aufgabe 2 AB7 Fertig Aufgabe 3 Später erst

--- src/Prog1/AB7/Aufgabe2/IntArrayUtil.java
+++ src/Prog1/AB7/Aufgabe2/IntArrayUtil.java
@@ -57,10 +57,115 @@ public class IntArrayUtil {
     }
 
     public static boolean containsMultiple(int[] a) {
+        for(int i = 0; i < a.length - 1; i++) {
+            for(int j = i + 1; j < a.length + 1; j++) {
+                if(a[i] == a[j]) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     public static int pos(int[] a, int n) {
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] == n) {
+                return i;
+            }
+        }
         return -1;
+    }
+
+    public static int[] putRandom(int[] a, int von, int bis) {
+        if(von >= bis) {
+            System.out.println("von muss kleiner als bis sein");
+            return a;
+        }
+        Random rand = new Random();
+        for(int i = 0; i < a.length; i++) {
+            int x = rand.nextInt(bis - von) + von;
+            a[i] = x;
+        }
+        return a;
+    }
+
+    public static int[] createRandom(int a) {
+        Random rand = new Random();
+        if(a <= 0) {
+            return new int[0];
+        }
+        int x = rand.nextInt(a) + 1;
+        int[] b = new int[x];
+        putRandom(b, 0, 100);
+
+        return b;
+    }
+
+    public static int max(int[] a) {
+        int max = a[0];
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] >= max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+    public static int min(int[] a) {
+        int min = a[0];
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] <= min) {
+                min = a[i];
+            }
+        }
+        return min;
+    }
+
+    public static int sum(int[] a) {
+        int sum = 0;
+        for(int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+
+    public static boolean isSorted(int[] a) {
+        for(int i = 0; i < a.length - 1; i++) {
+            if(a[i] > a[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void swap(int[] a, int b , int c) {
+        if(b < 0 || c < 0 || b >= a.length || c >= a.length) {
+            return;
+        }
+        int temp = a[b];
+        a[b] = a[c];
+        a[c] = temp;
+    }
+
+    public static int[] merge(int[] a, int[] b) {
+        int[] merged = new int[a.length + b.length]; // Richtige Länge
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                merged[k++] = a[i++];
+            } else {
+                merged[k++] = b[j++];
+            }
+        }
+
+        while (i < a.length) {
+            merged[k++] = a[i++];
+        }
+
+        while (j < b.length) {
+            merged[k++] = b[j++];
+        }
+
+        return merged;
     }
 }
--- src/Prog1/AB7/Aufgabe2/IntArrayUtilTest.java
+++ src/Prog1/AB7/Aufgabe2/IntArrayUtilTest.java
@@ -1,5 +1,7 @@
 package Prog1.AB7.Aufgabe2;
 
+import javax.sound.midi.SysexMessage;
+
 public class IntArrayUtilTest {
     public static void main(String[] args) {
         //Arrays
@@ -42,6 +44,57 @@ public class IntArrayUtilTest {
 
         System.out.println("\n------------------------------\n");
 
+        System.out.println("Contains Multiples Test");
+        int [] arrayMultiple = {1,1,3,4,5,6,7,8,9,10};
+        System.out.println("Gibt 2. '1' im Array? : " + IntArrayUtil.containsMultiple(arrayMultiple));
 
+        System.out.println("\n------------------------------\n");
+
+        int [] arrayPos = {1,3,4,5,1,7,8,9,10};
+        System.out.println("Pos Test");
+        System.out.println("wo ist 7 im array?: " + IntArrayUtil.pos(arrayPos, 7));
+        System.out.println("wo ist 11 (gibt es nicht) im array?: " + IntArrayUtil.pos(arrayPos, 11));
+
+        System.out.println("\n------------------------------\n");
+
+        System.out.println("Put Random Test");
+        System.out.println("Array wird mit Zahlen zwischen 10 - 20 belegt: " + IntArrayUtil.toString(IntArrayUtil.putRandom(arrayPos, 10,20)));
+        System.out.println("unveränderter Array:" + IntArrayUtil.toString(IntArrayUtil.putRandom(arrayPos, 20,10)));
+
+
+        System.out.println("\n------------------------------\n");
+
+        System.out.println("Create Random Test");
+        System.out.println("Array mit zufälliger länge(0-10) und zufälligen Werten(0 - 100) wird erstellt:\n" + IntArrayUtil.toString(IntArrayUtil.createRandom(10)));
+
+        System.out.println("\n------------------------------\n");
+
+        System.out.println("Max Min Sum Test");
+        int[] arrayMinMaxSum = {1,2,3,4,5};
+        System.out.println("Array: " + IntArrayUtil.toString(arrayMinMaxSum));
+        System.out.println("Max Test " + IntArrayUtil.max(arrayMinMaxSum));
+        System.out.println("Min Test " + IntArrayUtil.min(arrayMinMaxSum));
+        System.out.println("Sum Test " + IntArrayUtil.sum(arrayMinMaxSum));
+
+        System.out.println("\n------------------------------\n");
+
+        System.out.println("Is Sorted Test");
+        System.out.println("Array: " + IntArrayUtil.toString(arrayMinMaxSum) + " " + IntArrayUtil.isSorted(arrayMinMaxSum));
+        int[] isSortedf = {1,3,2,6,3};
+        System.out.println("Array: " + IntArrayUtil.toString(isSortedf) + " " + IntArrayUtil.isSorted(isSortedf));
+
+        System.out.println("\n------------------------------\n");
+
+        System.out.println("Swap Test");
+        System.out.println("Array: " + IntArrayUtil.toString(isSortedf));
+        IntArrayUtil.swap(isSortedf, 1 , 2);
+        System.out.println("Array nach Swap Indizes 1,2: " + IntArrayUtil.toString(isSortedf));
+
+        System.out.println("\n------------------------------\n");
+
+        int[] merge1 = {1,2,3,4,5};
+        int[] merge2 = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Array: " + IntArrayUtil.toString(merge1) + " Array2: " + IntArrayUtil.toString(merge2));
+        System.out.println("Neuer Array: " + IntArrayUtil.toString(IntArrayUtil.merge(merge2,merge1)));
     }
 }
