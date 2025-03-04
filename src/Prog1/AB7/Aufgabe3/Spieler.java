dd85c74ad080919a123db94c3a4e341c86e27895
HaenchenFlo <haenchenflorian@gmail.com>
2025-03-04 22:33:29 +0100

    Upload Aufgaben files mit Test (von der Aufgabe gegeben) Implementierung von Spiel erfolgt

---
+++ src/Prog1/AB7/Aufgabe3/Spieler.java
@@ -0,0 +1,26 @@
+package Prog1.AB7.Aufgabe3;
+
+import java.util.Random;
+
+public class Spieler {
+	
+	private static Random r = new Random();
+	private String name;
+	
+	public Spieler(String name) {
+		this.name = name;
+	}
+	
+	public String name() {
+		return name;
+	}
+	
+	public String toString() {
+		return name;
+	}
+	
+	public int spielen() {
+		return r.nextInt(100);
+	}
+
+}
