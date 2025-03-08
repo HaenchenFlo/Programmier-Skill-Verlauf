04c6afe68287661d5e27878c606d219cdb680d1c
Florian Hänchen <haenchenflorian@gmail.com>
2025-03-08 03:21:50 +0100

    Spiel fertig implementiert ggf testfälle hinzufügen klappt bisher perfekt -- review

--- src/Prog1/AB7/Aufgabe3/Spiel.java
+++ src/Prog1/AB7/Aufgabe3/Spiel.java
@@ -6,7 +6,7 @@ public class Spiel {
     private Spieler[] teilnehmerFeld;
 
 
-    public static int[] highscore = new int[11];
+    public static Ergebnis[] highscore = new Ergebnis[10];
 
 
     public Spiel(Spieler[] teilnehmerFeld) {
@@ -26,21 +26,44 @@ public class Spiel {
             int x = rand.nextInt(100);
             Ergebnis ergebnis = new Ergebnis(spieler, x);
 
-            System.out.println("Spieler: " + spieler + " hat das Ergebnis: " + ergebnis  + " erziehlt");
+            System.out.println(ergebnis  + " Punkte erziehlt");
 
             eintragen(ergebnis);
         }
     }
 
     private void eintragen(Ergebnis ergebnis) {
+        //wenn highscore leer dann einfach eintragen
+        if(highscore[0] == null) {
+            highscore[0] = ergebnis;
+            return;
+        }
+        int pos = -1;
+        for(int i = 0; i < highscore.length - 1; i++) {
+            if(highscore[i] == null || ergebnis.punkte() > highscore[i].punkte()) {
+                pos = i;
+                break;
+            }
+        }
 
+        if(pos == -1) {
+            return;
+        }
+
+        for(int i = highscore.length - 2; i >= pos; i--) {
+            highscore[i + 1] = highscore[i];
+        }
+
+        highscore[pos] = ergebnis;
     }
 
 
 
-    private void ausgabe() {
+    public static void ausgabe() {
         for(int i = 0; i < highscore.length - 1; i++) {
-            System.out.println(highscore[i]);
+            if(highscore[i] != null) {
+                System.out.println(highscore[i]);
+            }
         }
     }
 }
