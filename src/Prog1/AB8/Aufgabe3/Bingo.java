64fd3a8a9385cb15a405a1b91b09331896461693
HaenchenFlo <haenchenflorian@gmail.com>
2025-03-12 20:43:37 +0100

    start bingo

--- src/Prog1/AB8/Aufgabe3/Bingo.java
+++ src/Prog1/AB8/Aufgabe3/Bingo.java
@@ -1,5 +1,18 @@
 package Prog1.AB8.Aufgabe3;
+import java.util.Random;
 
 public class Bingo {
 
+    public static int[][] erstellBingoKarte(int n) {
+        Random rand = new Random();
+        int[][] bingoKarte = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                int zahl;
+
+            }
+        }
+        return bingoKarte;
+    }
 }
