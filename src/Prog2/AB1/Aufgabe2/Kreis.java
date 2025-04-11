4d3a00c3b675131cbf4fc8067760d83c5104c3b2
HaenchenFlo <haenchenflorian@gmail.com>
2025-04-11 16:37:37 +0200

    einige methoden von Quadrat und Kreis

--- src/Prog2/AB1/Aufgabe2/Kreis.java
+++ src/Prog2/AB1/Aufgabe2/Kreis.java
@@ -1,29 +1,37 @@
 package Prog2.AB1.Aufgabe2;
 
 public class Kreis extends GeoObjekt2D {
-    public Punkt2D mittelpunkt() {
+    private double radius;
+    private Punkt2D mittelpunkt;
+    private final double pi = Math.PI;
 
+    public Kreis(double radius, Punkt2D mittelpunkt) {
+        this.radius = Math.abs(radius);
+        this.mittelpunkt = mittelpunkt;
+    }
+
+    public Punkt2D mittelpunkt() {
+        return this.mittelpunkt;
     }
 
     public double radius() {
-
+        return this.radius;
     }
-
 
 
     @Override
     public double umfang() {
-
-    }
+        return 2 * pi * radius;
+     }
 
     @Override
     public double flaeche() {
-
+        return pi * (Math.pow(radius, radius));
     }
 
     @Override
     public void multiply(double d) {
-
+        radius = radius * d;
     }
 
     @Override
@@ -33,11 +41,11 @@ public class Kreis extends GeoObjekt2D {
 
     @Override
     public Punkt2D position() {
-        return null;
+        return mittelpunkt;
     }
 
     @Override
     public void moveTo(Punkt2D a) {
-
+        this.mittelpunkt = a;
     }
 }
--- src/Prog2/AB1/Aufgabe2/Quadrat.java
+++ src/Prog2/AB1/Aufgabe2/Quadrat.java
@@ -1,34 +1,45 @@
 package Prog2.AB1.Aufgabe2;
 
+import java.awt.geom.PathIterator;
+
 public class Quadrat extends GeoObjekt2D {
+    private Punkt2D LU;
+    private double kante;
+
+    public Quadrat(Punkt2D LU, double kante) {
+        this.LU = LU;
+        this.kante = Math.abs(kante);
+    }
 
     public Punkt2D eckeLU() {
-
+        return this.LU;
     }
 
     public Punkt2D eckeRO() {
-
+        double x = LU.x() + kante;
+        double y = LU.y() + kante;
+        return new Punkt2D(x,y);
     }
 
     public double kanten() {
-
+        return this.kante;
     }
 
 
 
     @Override
     public double umfang() {
-        return 0;
+        return 4 * kante;
     }
 
     @Override
     public double flaeche() {
-        return 0;
+        return kante * kante;
     }
 
     @Override
     public void multiply(double d) {
-
+        this.kante = Math.abs(this.kante * d);
     }
 
     @Override
@@ -38,11 +49,11 @@ public class Quadrat extends GeoObjekt2D {
 
     @Override
     public Punkt2D position() {
-        return null;
+        return this.LU;
     }
 
     @Override
     public void moveTo(Punkt2D a) {
-
+        this.LU = a;
     }
 }
