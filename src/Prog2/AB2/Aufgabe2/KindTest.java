0b8244e297e6f2516355e61976f19bc16e93a7ea
HaenchenFlo <haenchenflorian@gmail.com>
2025-04-14 12:57:20 +0200

    A2 Fertig letzte Aufgabe folgt

---
+++ src/Prog2/AB2/Aufgabe2/KindTest.java
@@ -0,0 +1,50 @@
+package Prog2.AB2.Aufgabe2;
+
+import Prog2.AB2.Aufgabe1.Paar;
+
+public class KindTest {
+
+    public static Paar<Kind, Paar<Person, Person>> family(Kind k) {
+        return new Paar<>(k, k.getEltern());
+    }
+
+    public static int dAlter(Paar<Kind, Paar<Person, Person>> family) {
+        Kind kind = family.erstes();
+        Paar<Person, Person> eltern = family.zweites();
+
+        int gebJahrKind = kind.gebJahr();
+        int gebJahrVater = eltern.erstes().gebJahr();
+        int gebJahrMutter = eltern.zweites().gebJahr();
+
+        int vaterAlter = gebJahrKind - gebJahrVater;
+        int mutterAlter = gebJahrKind - gebJahrMutter;
+
+        return (vaterAlter + mutterAlter) / 2;
+    }
+
+
+    public static void main(String[] args) {
+        Person Marge = new Person("Marge", 1972);
+        Person Homer = new Person("Homer", 1970);
+        Person AbeDad = new Person("AbeDad", 1909);
+        Person AbeMom = new Person("AbeMom", 1919);
+        Person HerbMom = new Person("HerbMom", 1929);
+
+        Kind Lisa = new Kind("Lisa",2000,new Paar<>(Homer,Marge));
+        Kind Bart = new Kind("Bart", 2000, new Paar<>(Homer, Marge));
+
+        Kind Abe = new Kind("Abe", 1930, new Paar<>(AbeDad,AbeMom));
+        Kind Herb = new Kind("Herb", 1969, new Paar<>(Abe,HerbMom));
+
+        System.out.println("Sind Bart und Lisa geschwister? : " + Kind.geschwister(Lisa, Bart));
+        System.out.println("Sind Abe und Herb geschwister? : " + Kind.geschwister(Abe, Herb));
+        System.out.println(Bart);
+        System.out.println(Lisa);
+        System.out.println(Herb);
+        System.out.println(Abe);
+
+        System.out.println(family(Bart));
+        System.out.println(dAlter(family(Bart)));
+    }
+
+}
