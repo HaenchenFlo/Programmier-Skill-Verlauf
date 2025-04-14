c4f4df8bbc2efc74c153df2dfdf86b064f8faf3a
HaenchenFlo <haenchenflorian@gmail.com>
2025-04-14 12:01:13 +0200

    AB2 Prog2 angefangen: Aufgabe 1 fertig a2 in bearbeitung

---
+++ src/Prog2/AB2/Aufgabe1/Paar.java
@@ -0,0 +1,42 @@
+package Prog2.AB2.Aufgabe1;
+
+public class Paar<E, Z> {
+    private E erstes;
+    private Z zweites;
+
+    public Paar(E erstes, Z zweites) {
+        this.erstes = erstes;
+        this.zweites = zweites;
+    }
+
+    public E erstes() {
+        return this.erstes;
+    }
+
+    public Z zweites() {
+        return this.zweites;
+    }
+
+    public void setErstes(E e) {
+        this.erstes = e;
+    }
+
+    public void setZweites(Z z) {
+        this.zweites = z;
+    }
+
+
+    @Override
+    public String toString() {
+        return "(" + erstes + ", " + zweites + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(!(this.getClass().equals(o.getClass()))) return false;
+
+        Paar<?, ?> other =  (Paar<?, ?>) o;
+        return (this.erstes == null ? other.erstes == null : this.erstes.equals(other.erstes)) &&
+                (this.zweites == null ? other.zweites == null : this.zweites.equals(other.zweites));
+    }
+}
