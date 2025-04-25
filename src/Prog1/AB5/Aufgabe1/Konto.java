c06093e28aa5f0d77189f43238a9d1aff8f25130
HaenchenFlo <haenchenflorian@gmail.com>
2025-04-25 10:42:29 +0200

    Ab5 upload und start / prog2 Ab4 upload

---
+++ src/Prog1/AB5/Aufgabe1/Konto.java
@@ -0,0 +1,43 @@
+package Prog1.AB5.Aufgabe1;
+
+public class Konto {
+    private int kontoNummer;
+    private String name;
+    private double kontoStand;
+    private int buchungen;
+
+    public Konto(String name, int kontonummer) {
+        this.kontoNummer = kontonummer;
+        this.name = name;
+        this.kontoStand = 0.0;
+        this.buchungen = 0;
+    }
+
+    public void setName(String neuerName) {
+        this.name = neuerName;
+    }
+
+    public double getKontoStand() {
+        return kontoStand;
+    }
+
+    public int getBuchungen() {
+        return buchungen;
+    }
+
+    public double einzahlen(double betrag) {
+        kontoStand += betrag;
+        buchungen++;
+        return kontoStand;
+    }
+
+    public double auszahlen(double betrag) {
+        kontoStand -= betrag;
+        buchungen++;
+        return kontoStand;
+    }
+
+    public String toString() {
+        return "Name: " + name + ", Kontonummer: " + kontoNummer + ", Kontostand: " + kontoStand;
+    }
+}
