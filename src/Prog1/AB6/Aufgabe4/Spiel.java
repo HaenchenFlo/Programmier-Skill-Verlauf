784d107ff438f05f2156091cc5441b103ebafee7
HaenchenFlo <haenchenflorian@gmail.com>
2025-05-09 09:55:47 +0200

    Prog1 Ü6 A4 -bonus 1 / 3 fertig

--- src/Prog1/AB6/Aufgabe4/Spiel.java
+++ src/Prog1/AB6/Aufgabe4/Spiel.java
@@ -1,5 +1,7 @@
 package Prog1.AB6.Aufgabe4;
 
+import Prog1.AB5.Aufgabe4.Wuerfel;
+
 import java.util.Scanner;
 
 public class Spiel {
@@ -21,5 +23,54 @@ public class Spiel {
         }
         System.out.println("Spieler " + sp1.getName() + " Punktzahl: " + sp1.punkte() + " ------ Spieler " + sp2.getName() + " Punktzahl: " + sp2.punkte());
         sc.close();
+    }
+
+    public static void filzlaus(Spieler sp1, Spieler sp2) {
+        int versuchesp1 = 0;
+        int versuchesp2 = 0;
+        boolean sp1hateins = false;
+        boolean sp2hateins = false;
+
+        while(versuchesp1 < 9 && !sp1hateins) {
+            versuchesp1++;
+            int wurf = sp1.wuerfeln();
+            if(wurf == 1) {
+                sp1.punktPlus(versuchesp1);
+                sp1hateins = true;
+            }
+        }
+
+        while(versuchesp2 < 9 && !sp2hateins) {
+            versuchesp2++;
+            int wurf = sp2.wuerfeln();
+            if(wurf == 1) {
+                sp2.punktPlus(versuchesp2);
+                sp2hateins = true;
+            }
+        }
+
+
+        if (sp1.punkte() == 0 && sp2.punkte() == 0) {
+            System.out.println("Beide Spieler haben keine 1 gewürfelt. Beide haben verloren!");
+            return;
+        }
+
+        if (sp1.punkte() == 0) {
+            System.out.println("Spieler " + sp1.getName() + " hat keine 1 gewürfelt und verliert.");
+            System.out.println("Spieler " + sp2.getName() + " gewinnt!");
+            return;
+        }
+
+        if (sp2.punkte() == 0) {
+            System.out.println("Spieler " + sp2.getName() + " hat keine 1 gewürfelt und verliert.");
+            System.out.println("Spieler " + sp1.getName() + " gewinnt!");
+            return;
+        }
+
+        if(sp1.punkte() < sp2.punkte()) {
+            System.out.println("Spieler " + sp1.getName() + " hat mit " + sp1.punkte() + " punkten gewonnen!");
+        } else {
+            System.out.println("Spieler " + sp2.getName() + " hat mit " + sp2.punkte() + " punkten gewonnen!");
+        }
     }
 }
--- src/Prog1/AB6/Aufgabe4/SpielTest.java
+++ src/Prog1/AB6/Aufgabe4/SpielTest.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class SpielTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Welches spiel willst du spielen? (1/2) :");
+        int n = sc.nextInt();
+        switch (n) {
+            case 1:
+                simplesSpielTest();
+                break;
+            case 2:
+                filzLausTest();
+                break;
+        }
+        sc.close();
+    }
+
+    public static void simplesSpielTest() {
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Gib deinen Namen ein: ");
         String name = sc.nextLine();
@@ -13,6 +28,29 @@ public class SpielTest {
         Spieler pc = new Spieler("Computer");
 
         Spiel.simplesSpiel(mensch, pc);
+
+        sc.close();
+    }
+
+    public static void filzLausTest() {
+        Scanner sc = new Scanner(System.in);
+
+        // Spielername eingeben
+        System.out.print("Bitte gib deinen Namen ein: ");
+        String name = sc.nextLine();
+
+        // Zwei Spieler erzeugen
+        Spieler mensch = new Spieler(name);
+        Spieler computer = new Spieler("Computer");
+
+        // Spiel starten
+        System.out.println("\n=== Spielstart: Filzlaus ===\n");
+        Spiel.filzlaus(mensch, computer);
+
+        // Punktestände anzeigen
+        System.out.println("\n=== Endstand ===");
+        System.out.println(mensch);
+        System.out.println(computer);
 
         sc.close();
     }
