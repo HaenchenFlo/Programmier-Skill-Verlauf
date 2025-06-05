120a5ecaa562fe3c44615a5cc3d4fb755daae678
HaenchenFlo <haenchenflorian@gmail.com>
2025-06-05 17:32:59 +0200

    Prog 2 AB5 start

---
+++ src/Prog2/AB5/Aufgabe1/Person.java
@@ -0,0 +1,13 @@
+package Prog2.AB5.Aufgabe1;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/Prog2/AB5/Aufgabe1/PostbotenIterator.java
@@ -0,0 +1,17 @@
+package Prog2.AB5.Aufgabe1;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class PostbotenIterator<T> implements Iterator<T> {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return null;
+    }
+}
