aa4e2872e8401c3f3729e3b1924f3fa683c33069
Florian Hänchen <haenchenflorian@gmail.com>
2025-07-20 10:20:31 +0200

    misc

--- src/Prog1/AltKlausur/Klausuren/SS21/A6.java
+++ src/Prog1/AltKlausur/Klausuren/SS21/A6.java
@@ -1,20 +1,20 @@
-package Prog1.AltKlausur.Klausuren.SS21;
-
-public class A6 {
-    public static void laufWeg(int n) {
-        Roboter alec = new Roboter();
-        int quad = 1;
-        for(int i = 0; i < n; i++) {
-            alec.step();
-            alec.turnL();
-            alec.step(quad);
-            alec.turnR();
-            alec.step(quad);
-            alec.turnR();
-            alec.step(quad);
-            alec.turnL();
-
-            quad *= 2;
-        }
-    }
-}
+//package Prog1.AltKlausur.Klausuren.SS21;
+//
+//public class A6 {
+//    public static void laufWeg(int n) {
+//        Roboter alec = new Roboter();
+//        int quad = 1;
+//        for(int i = 0; i < n; i++) {
+//            alec.step();
+//            alec.turnL();
+//            alec.step(quad);
+//            alec.turnR();
+//            alec.step(quad);
+//            alec.turnR();
+//            alec.step(quad);
+//            alec.turnL();
+//
+//            quad *= 2;
+//        }
+//    }
+//}
